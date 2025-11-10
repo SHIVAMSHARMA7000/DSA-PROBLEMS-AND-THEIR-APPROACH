05d2d653ce54d31d0493702b4cdfe97e286525a2
SHIVAM SHARMA <shivamsharma00006578@gmail.com>
2025-11-10 09:26:50 +0530

    Update boyer_moore.java

--- majority element/boyer_moore.java
+++ majority element/boyer_moore.java
@@ -2,7 +2,7 @@ public class boyer_moore {
     public static int majorityElement(int[] nums) {
       int freq = 0;
       int ans = 0;
-      for(int i = 1;i<nums.length;i++){
+      for(int i = 0;i<nums.length;i++){
         if(freq==0){
             ans = nums[i];
             
@@ -21,3 +21,4 @@ public class boyer_moore {
         System.out.print(majorityElement(a));
     }
 }
+
